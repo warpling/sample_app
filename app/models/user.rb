13a82b80f4a02953c3125775a98c5d5f86471d64
Ryan McLeod <ryanmcleod@gmail.com>
2010-07-23 01:12:57 -0700

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,13 @@
 #  updated_at :datetime
 #
 
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base 
   attr_accessible :name, :email
+
+  EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates_presence_of :name, :email
+  validates_length_of   :name, :maximum => 50
+  validates_format_of   :email, :with => EmailRegex
+  validates_uniqueness_of :email, :case_sensitive => false
 end
